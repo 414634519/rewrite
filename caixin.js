342a40f72fef12a863174cd8edbf1f98a0f31868
414634519 <139223400+414634519@users.noreply.github.com>
2024-11-30 15:51:49 +0800

    Update caixin.js

--- caixin.js
+++ caixin.js
@@ -1,11 +1,10 @@
 let body = $response.body;
 let obj = JSON.parse(body);
 
-// 检查是否有全文内容字段
-if (obj && obj.data && obj.data.content) {
-    // 解锁全文
-    obj.data.content = obj.data.content.replace(/<p class="paywall">.+?<\/p>/g, '');
+// 如果对象中包含 "paywall" 字段，将其设置为 false，确保显示全文
+if (obj && obj.data) {
     obj.data.paywall = false;
+    obj.data.content = obj.data.content.replace(/<p class="paywall">.*?<\/p>/g, '');
 }
 
 body = JSON.stringify(obj);
