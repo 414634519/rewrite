1b90e8c58956afd665d28c0ac53525c38f3e0bac
414634519 <139223400+414634519@users.noreply.github.com>
2024-12-01 14:15:09 +0800

    Update qinghua.js

--- qinghua.js
+++ qinghua.js
@@ -6,10 +6,10 @@ let obj = JSON.parse(body);
 obj.data.data.alumnus = true;
 
 // 增加新的字段
-obj.data.data.name = "刘俊杰";
-obj.data.data.enrollmentYear = 2020;
-obj.data.data.enrollmentType = "硕士";
-obj.data.data.department = "经济管理学院";
+obj.data.data.姓名 = "刘俊杰";
+obj.data.data.入校年份 = 2020;
+obj.data.data.入校类型 = "硕士";
+obj.data.data.院系 = "经济管理学院";
 
 // 重新生成 JSON 并返回
 body = JSON.stringify(obj);
